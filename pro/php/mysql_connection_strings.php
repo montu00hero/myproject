e2ab1c30412584d14fb5d29c35a4122bda9da8f8
montu00hero <montu00hero@gmail.com>
2016-05-26 20:03:21 +0530

    code for refrence

--- pro/php/mysql_connection_strings.php
+++ pro/php/mysql_connection_strings.php
@@ -4,11 +4,15 @@ $conn=mysql_connect('localhost','root','');
 
 var_dump($conn);
 
-$database_name=GDS;
+$database_name='GDS';
 
 mysql_select_db($database_name);
 
-$query=mysql_query('show table status ');
+$query=mysql_query('show table status ');  //give the info related to table
 
+while ( $row=  mysql_fetch_assoc($query))
+{
 
+echo"<pre>",print_r($row);
 //print_r($conn);
+}
